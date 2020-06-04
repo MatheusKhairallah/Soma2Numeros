8013b9c4a582ecb95a2ef2d199efd9f450c576ab
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:14:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um número:");
+            double numero1=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escreva outro número:");
+            double numero2=Convert.ToDouble(Console.ReadLine());
+            double resultado=numero1 + numero2;
+            Console.Clear();
+            Console.WriteLine($"Resultado da soma dos dois números digítados: {resultado}");
+            
+             
+
+
+            
+
+
+        }
+    }
+}
